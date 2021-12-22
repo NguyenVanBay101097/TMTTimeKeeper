d7e4abdfd0421def7b5fbd0656691bbd97665ddd
NguyenNhatLuan <nhatluan98@gmail.com>
2021-12-22 13:35:39 +0700

    push initial

---
+++ TMTTimeKeeper/Models/TimeKeeperModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TMTTimeKeeper.Models
+{
+    public class TimeKeeperSave
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string IPAddress { get; set; }
+        public string TCPPort { get; set; }
+    }
+}
